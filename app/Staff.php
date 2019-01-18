f7c5983c1db38227ef0d325c675b8edd0dc50979
Gerardo Merida[B <cgemerida@gmail.com>
2019-01-18 11:46:42 -0600

    Agrega el nombre de la tabla a staffs

--- app/Staff.php
+++ app/Staff.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
+    protected $table = 'staffs';
+    
     protected $fillable = [
         'dpi', 'name', 'last_name', 'rol'
     ];
