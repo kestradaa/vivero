0cce188cce005c8a47e06c37800dbf1a5bdd424a
Kevin Estrada <kestradaa17@gmail.com>
2019-04-09 16:42:57 -0600

    creando graficas

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -2,15 +2,23 @@
 
 @section('css')
 
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
 
 @endsection
 
 @section('content')
 
-    <div class="">
-        <div>
-            <h1>Bar charts</h1>
+    <div class="container">
+    	<h2>INDICADORES</h2>
+    	<hr>
+
+        <div class="row">
+        	<div class="col-md-6">
+        		<h4>Grafica de Linea</h4>
+        	</div>
+        	<div class="col-md-6">
+        		<h4>Grafica de Area</h4>
+        	</div>
         </div>
     </div>
 
@@ -18,8 +26,8 @@
 
 @section('js')
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
 @endsection
