3f54beed0e6a54b3acd0ede4a5d42cc55096c53f
Kevin Estrada <kestradaa17@gmail.com>
2019-04-11 17:24:50 -0600

    implementacion de graficas sin info de la base de datos3

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -10,17 +10,18 @@
 
     <div class="container">
     	<h2>INDICADORES</h2>
-    	<hr>
     	<br>
         <div class="row">
         	<div class="col-md-6">
         		<h3>Prueba 1</h3>
-        			<div id="prueba1" style="height: 250px;"></div>
+        		<hr>
+        			<div id="prueba1"></div>
         	</div>
         	<div class="col-md-6">
         		<h3>Prueba 2</h3>
+        		<hr>
         		<div>
-        			<div id="prueba2" style="height: 250px;"></div>
+        			<div id="prueba2"></div>
         		</div>
         	</div>
         </div>
@@ -28,12 +29,14 @@
         <div class="row">
         	<div class="col-md-6">
         		<h3>Prueba 3</h3>
-        			<div id="prueba3" style="height: 250px;"></div>
+        		<hr>
+        			<div id="prueba3"></div>
         	</div>
         	<div class="col-md-6">
         		<h3>Prueba 4</h3>
+        		<hr>
         		<div>
-        			<div id="prueba4" style="height: 250px;"></div>
+        			<div id="prueba4"></div>
         		</div>
         	</div>
         </div>
@@ -51,6 +54,7 @@
     	
     	Morris.Donut({
 		    element: 'prueba1',
+		    resize: true,
 		    data: [
 		        {value: 5, label: 'foo'},
 		        {value: 15, label: 'bar'},
@@ -64,6 +68,7 @@
 
 		Morris.Bar({
 			element: 'prueba2',
+			resize: true,
 			data: [
 			    {x: '2011 Q1', y: 1},
 			    {x: '2011 Q2', y: 2},
@@ -91,6 +96,7 @@
 
 		Morris.Area({
   			element: 'prueba4',
+  			resize: true,
 			data: [
     			{x: '2010 Q4', y: 3, z: 7},
     			{x: '2011 Q1', y: 3, z: 4},
@@ -119,13 +125,28 @@
 			{"period": "2012-09-10", "licensed": 3215, "sorned": 622}
 		];
 
-		Morris.Bar({
+		new Morris.Line({
+			// ID of the element in which to draw the chart.
 			element: 'prueba3',
-			data: day_data,
-			xkey: 'period',
-			ykeys: ['licensed', 'sorned'],
-			labels: ['Licensed', 'SORN'],
-			xLabelAngle: 60
+			// Chart data records -- each entry in this array corresponds to a point on
+			// the chart.
+			data: [
+			    { year: '2015', value: 20, value2: 10},
+			    { year: '2016', value: 10, value2: 5 },
+			    { year: '2017', value: 5, value2: 20 },
+			    { year: '2018', value: 5, value2: 30 },
+			    { year: '2019', value: 20, value2: 7 }
+			],
+			// The name of the data record attribute that contains x-values.
+			xkey: 'year',
+			// A list of names of data record attributes that contain y-values.
+			ykeys: ['value','value2'],
+			// Labels for the ykeys -- will be displayed when you hover over the
+			// chart.
+			labels: ['Coca Cola', 'Pepsi'],
+			resize: true,
+			lineColors: ['red', 'blue'],
+			lineWidth: 1
 		});
     </script>
 
